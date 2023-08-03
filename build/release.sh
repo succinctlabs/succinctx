6cd846da577137a1ca5bf7c82b27d4dab8b10466
mattstam <mattstam@live.com>
2023-08-03 16:48:43 -0700

    release.sh for always nightly

--- build/release.sh
+++ build/release.sh
@@ -7,7 +7,14 @@ if ! echo "$1" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+(-rc\.[0-9]+)?$'; then
   exit 1
 fi
 
-# Create a new signed annotated tag
+# Create a new signed annotated tag for the version
 git tag "v${1}" -as -m "release v${1}"
-# Push it, which triggers the Release workflow
-git push --atomic origin "v${1}"
+
+# Create or update the "latest" tag
+git tag -af "latest" -m "latest release" "v${1}"
+
+# Create or update the "nightly" tag
+git tag -af "nightly" -m "nightly release" "v${1}"
+
+# Push all tags, which triggers the Release workflow
+git push --atomic origin "v${1}" "latest" "nightly"
