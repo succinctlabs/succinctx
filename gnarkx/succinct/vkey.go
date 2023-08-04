b687ad267293e125b303240f8fe197e3992c54ef
mattstam <mattstam@live.com>
2023-08-04 15:56:42 -0700

    fix arr size 2

--- gnarkx/succinct/vkey.go
+++ gnarkx/succinct/vkey.go
@@ -28,7 +28,7 @@ func (svk *SuccinctVerifyingKey) ExportIFunctionVerifierSolidity(w io.Writer) er
 	content = newLicense + content
 
 	// Custom replacements to make compatible with IFunctionVerifier.
-	content = strings.ReplaceAll(content, "uint256[65] calldata input", "uint256[65] memory input")
+	content = strings.ReplaceAll(content, "uint256[2] calldata input", "uint256[2] memory input")
 	content = strings.ReplaceAll(content, "pragma solidity ^0.8.0;", "pragma solidity ^0.8.16;")
 	// write the new content to the writer
 	_, err = w.Write([]byte(content))
@@ -49,21 +49,9 @@ contract FunctionVerifier is IFunctionVerifier, Verifier {
         (uint256[2] memory a, uint256[2][2] memory b, uint256[2] memory c) =
             abi.decode(_proof, (uint256[2], uint256[2][2], uint256[2]));
 
-        uint256[65] memory input;
-        uint256 inputHashUint = uint256(_inputHash);
-        for (uint256 j = 0; j < 32; j++) {
-            input[32 - 1 - j] = inputHashUint % 2 ** 8;
-            inputHashUint = inputHashUint / 2 ** 8;
-        }
-        uint256 outputHashUint = uint256(_outputHash);
-        for (uint256 j = 0; j < 32; j++) {
-            input[64 - j] = outputHashUint % 2 ** 8;
-            outputHashUint = outputHashUint / 2 ** 8;
-        }
-        // TODO:
-        // input[32] =
+		uint256[2] memory input = [uint256(_inputHash), uint256(_outputHash)];
 
-        return verifyProof(a, b, c, input);
+		return verifyProof(a, b, c, input);
     }
 
     function verificationKeyHash() external pure returns (bytes32) {
