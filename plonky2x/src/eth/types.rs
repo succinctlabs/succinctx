51e4d907532fbbe6e3c80e754d9c8a7ccbf6a1ae
Uma Roy <uma.roy.us@gmail.com>
2023-08-07 19:45:48 -0700

    Adding storage proof API

--- plonky2x/src/eth/types.rs
+++ plonky2x/src/eth/types.rs
@@ -4,6 +4,9 @@ use crate::vars::BoolVariable;
 #[derive(Debug, Clone, Copy)]
 pub struct BLSPubkeyVariable(pub [BoolVariable; 512]);
 
+#[derive(Debug, Clone, Copy)]
+pub struct AddressVariable(pub [BoolVariable; 160]);
+
 impl BuilderAPI {
     /// Initialize a new BLSPubkeyVariable.
     pub fn init_bls_pubkey(&mut self) -> BLSPubkeyVariable {
--- plonky2x/src/vars/mod.rs
+++ plonky2x/src/vars/mod.rs
@@ -3,9 +3,11 @@ mod byte;
 mod bytes32;
 mod u256;
 mod variable;
+mod bytes;
 
 pub use boolean::BoolVariable;
 pub use byte::ByteVariable;
 pub use bytes32::Bytes32Variable;
 pub use u256::U256Variable;
 pub use variable::Variable;
+pub use bytes::BytesVariable;
