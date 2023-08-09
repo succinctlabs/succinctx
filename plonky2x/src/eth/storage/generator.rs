584d67a2de70dc3b52b781aa9de03be876f3d814
Uma Roy <uma.roy.us@gmail.com>
2023-08-09 14:40:50 -0700

    More fixes

--- plonky2x/src/eth/storage/generator.rs
+++ plonky2x/src/eth/storage/generator.rs
@@ -4,13 +4,11 @@ use plonky2::field::extension::Extendable;
 use plonky2::hash::hash_types::RichField;
 use plonky2::iop::generator::{GeneratedValues, SimpleGenerator};
 use plonky2::iop::target::Target;
-use plonky2::iop::witness::{PartitionWitness};
+use plonky2::iop::witness::PartitionWitness;
 use plonky2::plonk::circuit_data::CommonCircuitData;
 use plonky2::util::serialization::{Buffer, IoResult};
 
-use ethers::providers::{Http, Middleware, Provider};
-
-
+use ethers::providers::{Http, Provider};
 
 use crate::vars::{Bytes32Variable, WitnessMethods};
 use crate::eth::types::{AddressVariable};
