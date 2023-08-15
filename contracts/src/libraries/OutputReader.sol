c0f4b98180ca2f68851c7c8413a61a0fde2c7124
mattstam <mattstam@live.com>
2023-08-14 17:45:51 -0700

    include output reader fix

--- contracts/src/libraries/OutputReader.sol
+++ contracts/src/libraries/OutputReader.sol
@@ -8,4 +8,12 @@ library OutputReader {
         }
         return (_offset + 32, value);
     }
+
+    function readUint64(bytes memory _output, uint256 _offset) internal pure returns (uint256, uint64) {
+        uint64 value;
+        assembly {
+            value := mload(add(add(_output, 0x08), _offset))
+        }
+        return (_offset + 8, value);
+    }
 }
