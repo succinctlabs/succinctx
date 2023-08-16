a7e3d71b1817425ef48ef953cda75827998c6e06
mattstam <mattstam@live.com>
2023-08-16 11:21:47 -0700

    refactor(contracts): base IFunctionVerifier interface

--- contracts/src/interfaces/IFunctionVerifier.sol
+++ contracts/src/interfaces/IFunctionVerifier.sol
@@ -3,6 +3,9 @@ pragma solidity >=0.5.0;
 
 interface IFunctionVerifier {
     function verify(bytes32 inputHash, bytes32 outputHash, bytes memory proof) external returns (bool);
+}
 
+interface ICircuitVerifier is IFunctionVerifier {
+    function verify(bytes32 inputHash, bytes32 outputHash, bytes memory proof) external returns (bool);
     function verificationKeyHash() external view returns (bytes32);
 }
