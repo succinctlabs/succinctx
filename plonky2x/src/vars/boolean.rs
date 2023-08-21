0514d95c84617569c1f565db7b3412348567d2ef
Tamir Hemo <tamir@succinct.xyz>
2023-08-21 11:57:43 -0700

    bool

--- plonky2x/src/vars/boolean.rs
+++ plonky2x/src/vars/boolean.rs
@@ -67,7 +67,9 @@ impl<F: RichField + Extendable<D>, const D: usize> BitOr<F, D> for BoolVariable 
     type Output = BoolVariable;
 
     fn bitor(self, rhs: BoolVariable, builder: &mut CircuitBuilder<F, D>) -> Self::Output {
-        builder.add(self.0, rhs.0).into()
+        let self_plus_rhs = builder.add(self.0, rhs.0);
+        let self_times_rhs = builder.mul(self.0, rhs.0);
+        builder.sub(self_plus_rhs, self_times_rhs).into()
     }
 }
 
@@ -88,5 +90,60 @@ impl<F: RichField + Extendable<D>, const D: usize> Not<F, D> for BoolVariable {
     fn not(self, builder: &mut CircuitBuilder<F, D>) -> Self::Output {
         let one = builder.one::<Variable>();
         builder.sub(one, self.0).into()
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use plonky2::field::goldilocks_field::GoldilocksField;
+    use plonky2::iop::witness::PartialWitness;
+    use plonky2::plonk::config::PoseidonGoldilocksConfig;
+
+    use crate::builder::CircuitBuilder;
+    use super::*;
+
+    #[test]
+    fn test_bit_ops() {
+        type F = GoldilocksField;
+        type C = PoseidonGoldilocksConfig;
+        const D: usize = 2;
+
+        let mut builder = CircuitBuilder::<F, D>::new();
+
+        let x = builder.init::<BoolVariable>();
+        let y = builder.init::<BoolVariable>();
+
+        let not_x = builder.not(x);
+        let not_y = builder.not(y);
+        let x_and_y = builder.and(x, y);
+        let x_and_x = builder.and(x, x);
+        let x_or_y = builder.or(x, y);
+        let x_or_x = builder.or(x, x);
+        let y_or_y = builder.or(y, y);
+        let x_xor_y = builder.xor(x, y);
+        let x_xor_x = builder.xor(x, x);
+        let y_xor_y = builder.xor(y, y);
+
+        let mut pw = PartialWitness::new();
+
+        x.set(&mut pw, true);
+        y.set(&mut pw, false);
+
+        not_x.set(&mut pw, false);
+        not_y.set(&mut pw, true);
+        x_and_y.set(&mut pw, false);
+        x_and_x.set(&mut pw, true);
+        x_or_y.set(&mut pw, true);
+        x_or_x.set(&mut pw, true);
+        y_or_y.set(&mut pw, false);
+        x_xor_y.set(&mut pw, true);
+        x_xor_x.set(&mut pw, false);
+        y_xor_y.set(&mut pw, false);
+
+        let data = builder.build::<C>();
+        let proof = data.prove(pw).unwrap();
+        data.verify(proof).unwrap();
     }
 }
