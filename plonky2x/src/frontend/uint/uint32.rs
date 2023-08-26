87e1f7b6fb65ad3085fbc7b821e88a26b2b9e2be
Chris Tian <chris@succinct.xyz>
2023-08-26 11:44:09 -0700

    feat: u32 evmvariable

--- plonky2x/src/frontend/uint/uint32.rs
+++ plonky2x/src/frontend/uint/uint32.rs
@@ -2,10 +2,12 @@ use std::fmt::Debug;
 
 use plonky2::field::extension::Extendable;
 use plonky2::hash::hash_types::RichField;
+use plonky2::iop::target::BoolTarget;
 use plonky2::iop::witness::{Witness, WitnessWrite};
 
 use crate::frontend::builder::CircuitBuilder;
-use crate::frontend::vars::{CircuitVariable, Variable};
+use crate::frontend::vars::{CircuitVariable, EvmVariable, Variable};
+use crate::prelude::{BoolVariable, ByteVariable, Zero};
 
 /// A variable in the circuit representing a u32 value. Under the hood, it is represented as
 /// a single field element.
@@ -44,5 +46,62 @@ impl CircuitVariable for U32Variable {
 
     fn set<F: RichField, W: WitnessWrite<F>>(&self, witness: &mut W, value: Self::ValueType<F>) {
         witness.set_target(self.0 .0, F::from_canonical_u32(value));
+    }
+}
+
+impl EvmVariable for U32Variable {
+    fn encode<F: RichField + Extendable<D>, const D: usize>(
+        &self,
+        builder: &mut CircuitBuilder<F, D>,
+    ) -> Vec<ByteVariable> {
+        let mut bytes = vec![];
+        let bits = builder.api.split_le(self.0 .0, 32);
+        for i in (0..4).rev() {
+            let mut arr: [BoolVariable; 8] = [builder._false(); 8];
+            let byte = bits[i * 8..(i + 1) * 8].to_vec();
+            byte.iter()
+                .rev()
+                .enumerate()
+                .try_fold(&mut arr, |arr, (j, &bit)| {
+                    arr[j] = BoolVariable(Variable(bit.target));
+                    Some(arr)
+                });
+            bytes.push(ByteVariable(arr));
+        }
+        bytes
+    }
+
+    fn decode<F: RichField + Extendable<D>, const D: usize>(
+        builder: &mut CircuitBuilder<F, D>,
+        bytes: &[ByteVariable],
+    ) -> Self {
+        assert_eq!(bytes.len(), 4);
+        let mut bits = vec![];
+        for byte in bytes.iter() {
+            bits.extend_from_slice(&byte.0);
+        }
+        let target = builder.api.le_sum(
+            bits.iter()
+                .rev()
+                .map(|bit| BoolTarget::new_unsafe(bit.0 .0)),
+        );
+        Self(Variable(target))
+    }
+
+    fn encode_value<F: RichField>(value: Self::ValueType<F>) -> Vec<u8> {
+        let mut bytes = vec![0_u8; 4];
+        for i in 0..4 {
+            bytes[i] = ((value >> ((4 - i - 1) * 8)) & 0xff) as u8;
+        }
+        bytes
+    }
+
+    fn decode_value<F: RichField>(bytes: &[u8]) -> Self::ValueType<F> {
+        assert_eq!(bytes.len(), 4);
+        let mut value = 0_u32;
+        for i in 0..4 {
+            value |= (bytes[i] as u32) << ((4 - i - 1) * 8);
+        }
+        value
     }
 }
