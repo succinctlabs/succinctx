c71cae7b772de37fb9d93889d49c3e1011359f87
Uma Roy <uma.roy.us@gmail.com>
2023-08-27 19:06:02 -0700

    more helpful error message

--- plonky2x/src/utils/eth/mod.rs
+++ plonky2x/src/utils/eth/mod.rs
@@ -28,7 +28,8 @@ impl From<Vec<u8>> for BLSPubkey {
 pub fn get_provider(chain_id: u64) -> Provider<Http> {
     dotenv::dotenv().ok();
     let rpc_str = format!("RPC_{}", chain_id);
-    let rpc_url = env::var(rpc_str).unwrap();
+    let rpc_url = env::var(rpc_str)
+        .expect(format!("RPC_{} environment variable was not found", chain_id).as_str());
     let provider = Provider::<Http>::try_from(rpc_url).unwrap();
     provider
 }
