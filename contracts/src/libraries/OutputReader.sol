cfc0a7e9b02589d620ae24af82d5917395b5805d
Chris Tian <chris@succinct.xyz>
2023-08-28 20:41:50 -0700

    feat: add uint32/128 to OutputReader

--- contracts/src/libraries/OutputReader.sol
+++ contracts/src/libraries/OutputReader.sol
@@ -9,11 +9,27 @@ library OutputReader {
         return (_offset + 32, value);
     }
 
+    function readUint128(bytes memory _output, uint256 _offset) internal pure returns (uint256, uint128) {
+        uint128 value;
+        assembly {
+            value := mload(add(add(_output, 0x10), _offset))
+        }
+        return (_offset + 16, value);
+    }
+
     function readUint64(bytes memory _output, uint256 _offset) internal pure returns (uint256, uint64) {
         uint64 value;
         assembly {
             value := mload(add(add(_output, 0x08), _offset))
         }
         return (_offset + 8, value);
+    }
+
+    function readUint32(bytes memory _output, uint256 _offset) internal pure returns (uint256, uint32) {
+        uint32 value;
+        assembly {
+            value := mload(add(add(_output, 0x04), _offset))
+        }
+        return (_offset + 4, value);
     }
 }
