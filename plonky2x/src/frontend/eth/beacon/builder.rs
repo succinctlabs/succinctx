6f984bda3f6a69700022c95477243fc810568d9d
Ameya <f20200139@pilani.bits-pilani.ac.in>
2023-08-31 04:48:18 +0530

    initial commit for ssz merkle proof

--- plonky2x/src/frontend/eth/beacon/builder.rs
+++ plonky2x/src/frontend/eth/beacon/builder.rs
@@ -1,11 +1,16 @@
+use array_macro::array;
 use plonky2::field::extension::Extendable;
 use plonky2::hash::hash_types::RichField;
+use plonky2::iop::target::BoolTarget;
+
 
 use super::generators::validator::BeaconValidatorGenerator;
 use super::vars::{BeaconValidatorVariable, BeaconValidatorsVariable};
 use crate::frontend::builder::CircuitBuilder;
 use crate::frontend::eth::beacon::generators::validators::BeaconValidatorsRootGenerator;
-use crate::frontend::vars::Bytes32Variable;
+use crate:: frontend::hash::sha::sha256;
+use crate::frontend::vars::{Bytes32Variable, ByteVariable, BytesVariable, BoolVariable};
+use crate::frontend::vars::CircuitVariable;
 
 impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
     /// Get the validators for a given block root.
@@ -40,7 +45,47 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
         self.add_simple_generator(&generator);
         generator.validator
     }
+
+    /// SHA256 implementation for builder 
+        pub fn sha(&mut self, input: &[ByteVariable]) -> Bytes32Variable {
+            let input_bool: Vec<BoolTarget> = input.iter().flat_map(|byte| byte.0.iter().cloned().map(|bool_variable| BoolTarget::new_unsafe(bool_variable.0 .0))).collect();
+            let hash_bool = sha256::sha256::<F,D>(&mut self.api, &input_bool);
+            let hash_bytes_vec = hash_bool.chunks(8).map(|chunk| ByteVariable(array![i => BoolVariable::from(chunk[i].target); 8])).collect::<Vec<_>>();
+            let mut hash_bytes_array = [ByteVariable::init(self); 32];
+            hash_bytes_array.copy_from_slice(&hash_bytes_vec);
+            Bytes32Variable(BytesVariable(hash_bytes_array))
+        }
+    
+    /// SSZ Merkle Proof 
+    pub fn ssz_restore_merkle_root(
+        &mut self,
+        leaf: Bytes32Variable,
+        index: u64,
+        depth: usize,
+        branch: Vec<Bytes32Variable>,
+    ) -> Bytes32Variable {
+        let mut hasher = leaf; // Initialize the hasher with the leaf node
+        for i in 0..depth { // Iterate over each level of the Merkle tree
+            let (first, second) = if (index >> i) & 1 == 1 { 
+                // Determine the order of hashing based on the index
+                (branch[i].as_bytes(), hasher.as_bytes())
+            } else {
+                (hasher.as_bytes(), branch[i].as_bytes())
+            };
+            let data: [ByteVariable; 64] = first.iter()
+                .chain(second.iter())
+                .cloned()
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(); // Combine the first and second slices into a single array
+            hasher = self.sha(&data); // Compute the hash of the data
+        }
+        hasher // Return the computed Merkle root
+    }
+
 }
+    
+    
 
 #[cfg(test)]
 pub(crate) mod tests {
