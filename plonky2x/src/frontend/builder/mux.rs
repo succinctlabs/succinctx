4e387c7ebbd48626760c6625fdff02760c014e4b
0x0 <coollightning123@gmail.com>
2023-09-05 15:56:29 +0800

    proptest run too long

--- plonky2x/src/frontend/builder/mux.rs
+++ plonky2x/src/frontend/builder/mux.rs
@@ -35,37 +35,37 @@ mod test {
     use plonky2::field::types::Field;
     use plonky2::iop::witness::PartialWitness;
     use plonky2::plonk::config::PoseidonGoldilocksConfig;
-    use rand::rngs::OsRng;
-    use rand::Rng;
+    use proptest::prelude::*;
 
     use crate::frontend::builder::mux::MuxBuilder;
     use crate::frontend::vars::U256Variable;
     use crate::prelude::{CircuitBuilder, CircuitVariable, GoldilocksField, Variable};
 
-    #[test]
-    fn test_mux() {
-        type F = GoldilocksField;
-        type C = PoseidonGoldilocksConfig;
-        const D: usize = 2;
-
-        let mut builder = CircuitBuilder::<F, D>::new();
-        let five = builder.constant::<Variable>(F::from_noncanonical_u64(5));
-
-        // random inputs
-        let mut rng = OsRng;
-        let inputs = rng.gen::<[u64; 10]>();
-        let input_variables = inputs
-            .iter()
-            .map(|x| U256Variable::constant(&mut builder, U256::from(*x)))
-            .collect::<Vec<_>>();
-
-        let output = builder.select_index(five, &input_variables[..]);
-
-        builder.assert_is_equal(output, input_variables[5]);
-
-        let circuit = builder.build::<C>();
-        let pw = PartialWitness::new();
-        let proof = circuit.data.prove(pw).unwrap();
-        circuit.data.verify(proof).unwrap();
+    proptest! {
+        #[test]
+        fn test_mux(inputs in prop::collection::vec(any::<u64>(), 0..=5)) {
+            type F = GoldilocksField;
+            type C = PoseidonGoldilocksConfig;
+            const D: usize = 2;
+    
+            let mut builder = CircuitBuilder::<F, D>::new();
+            for index in 0..inputs.len() {
+                let index_var = builder.constant::<Variable>(F::from_noncanonical_u64(index.try_into().unwrap()));
+    
+                let input_variables = inputs
+                    .iter()
+                    .map(|x| U256Variable::constant(&mut builder, U256::from(*x)))
+                    .collect::<Vec<_>>();
+        
+                let output = builder.select_index(index_var, &input_variables[..]);
+        
+                builder.assert_is_equal(output, input_variables[index]);
+            }
+          
+            let circuit = builder.build::<C>();
+            let pw = PartialWitness::new();
+            let proof = circuit.data.prove(pw).unwrap();
+            circuit.data.verify(proof).unwrap();
+        }
     }
 }
