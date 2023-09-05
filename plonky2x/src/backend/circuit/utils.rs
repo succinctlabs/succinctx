7500aaf7073d7586131997c291cabf10320f9fcb
John Guibas <john@succinct.xyz>
2023-09-05 14:02:52 -0700

    u32 arithmetic generator

--- plonky2x/src/backend/circuit/utils.rs
+++ plonky2x/src/backend/circuit/utils.rs
@@ -49,7 +49,7 @@ use crate::frontend::hash::bit_operations::{XOR3Gate, XOR3Generator};
 use crate::frontend::hash::keccak::keccak256::Keccak256Generator;
 use crate::frontend::num::biguint::BigUintDivRemGenerator;
 use crate::frontend::num::u32::gates::add_many_u32::{U32AddManyGate, U32AddManyGenerator};
-use crate::frontend::num::u32::gates::arithmetic_u32::U32ArithmeticGate;
+use crate::frontend::num::u32::gates::arithmetic_u32::{U32ArithmeticGate, U32ArithmeticGenerator};
 use crate::frontend::num::u32::gates::comparison::ComparisonGate;
 
 #[macro_export]
@@ -178,6 +178,7 @@ where
         BeaconWithdrawalsGenerator<F, D>, "BeaconWithdrawalsGenerator",
         BeaconHistoricalBlockGenerator<F, D>, "BeaconHistoricalBlockGenerator",
         BigUintDivRemGenerator<F, D>, "BigUintDivRemGenerator",
+        U32ArithmeticGenerator<F, D>, "U32ArithmeticGenerator",
     }
 }
 
