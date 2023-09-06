19d86cc34f97287258808246e894897059bc5a44
Uma Roy <uma.roy.us@gmail.com>
2023-09-05 19:12:28 -0700

    Removed another generator that was unused

--- plonky2x/src/frontend/eth/mpt/reference/generators.rs
+++ plonky2x/src/frontend/eth/mpt/reference/generators.rs
@@ -94,54 +94,6 @@ impl<F: RichField + Extendable<D>, const D: usize, V: CircuitVariable, const N: 
 }
 
 #[derive(Debug, Clone)]
-pub struct NestedMuxGenerator<F: RichField + Extendable<D>, const D: usize, const N: usize> {
-    pub input: Vec<[ByteVariable; N]>,
-    pub output: [ByteVariable; N],
-    pub selector: Variable,
-    pub _phantom: PhantomData<F>,
-}
-
-impl<F: RichField + Extendable<D>, const D: usize, const N: usize> SimpleGenerator<F, D>
-    for NestedMuxGenerator<F, D, N>
-{
-    fn id(&self) -> String {
-        "MuxGenerator".to_string()
-    }
-
-    fn dependencies(&self) -> Vec<Target> {
-        let mut targets: Vec<Target> = Vec::new();
-        for i in 0..self.input.len() {
-            targets.extend(
-                self.input[i]
-                    .iter()
-                    .map(|x| x.targets())
-                    .flatten()
-                    .collect::<Vec<Target>>(),
-            );
-        }
-        targets.extend(self.selector.targets());
-        targets
-    }
-
-    fn run_once(&self, witness: &PartitionWitness<F>, out_buffer: &mut GeneratedValues<F>) {
-        let selector = self.selector.get(witness).to_canonical_u64() as usize;
-        for i in 0..N {
-            self.output[i].set(out_buffer, self.input[selector][i].get(witness));
-        }
-    }
-
-    #[allow(unused_variables)]
-    fn serialize(&self, dst: &mut Vec<u8>, common_data: &CommonCircuitData<F, D>) -> IoResult<()> {
-        todo!()
-    }
-
-    #[allow(unused_variables)]
-    fn deserialize(src: &mut Buffer, common_data: &CommonCircuitData<F, D>) -> IoResult<Self> {
-        todo!()
-    }
-}
-
-#[derive(Debug, Clone)]
 pub struct ByteSubGenerator<F: RichField + Extendable<D>, const D: usize> {
     pub lhs: ByteVariable,
     pub rhs: ByteVariable,
