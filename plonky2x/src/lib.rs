bfa4d56a5e254b93dd6a45acf1dcf2b370fae68a
Uma Roy <uma.roy.us@gmail.com>
2023-09-11 14:32:52 -0700

    merge conflicts again

--- plonky2x/src/lib.rs
+++ plonky2x/src/lib.rs
@@ -25,9 +25,7 @@ pub mod prelude {
     pub use plonky2x_derive::CircuitVariable;
 
     pub use crate::backend::circuit::config::{DefaultParameters, PlonkParameters};
-    pub use crate::backend::circuit::{
-        GateRegistry, PlonkParameters, WitnessGeneratorRegistry, WitnessGeneratorRegistry,
-    };
+    pub use crate::backend::circuit::{GateRegistry, WitnessGeneratorRegistry};
     pub use crate::frontend::builder::{CircuitBuilder, DefaultBuilder};
     pub use crate::frontend::ops::*;
     pub use crate::frontend::vars::{
