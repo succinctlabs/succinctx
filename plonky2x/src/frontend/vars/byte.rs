4870b1136584fbf77b01ddace23b7a1e7f41eac9
Siddhant Sharma <sharmasiddhant301@gmail.com>
2023-09-11 23:01:42 -0700

    full subtractor

--- plonky2x/src/frontend/vars/byte.rs
+++ plonky2x/src/frontend/vars/byte.rs
@@ -9,7 +9,7 @@ use serde::{Deserialize, Serialize};
 use super::{BoolVariable, CircuitVariable, EvmVariable, Variable};
 use crate::backend::circuit::PlonkParameters;
 use crate::frontend::builder::CircuitBuilder;
-use crate::frontend::ops::{BitAnd, BitOr, BitXor, Not, RotateLeft, RotateRight, Shl, Shr, Zero, Add};
+use crate::frontend::ops::{BitAnd, BitOr, BitXor, Not, RotateLeft, RotateRight, Shl, Shr, Zero, Add, Sub};
 
 /// A variable in the circuit representing a byte value. Under the hood, it is represented as
 /// eight bits stored in big endian.
@@ -249,6 +249,24 @@ impl<L: PlonkParameters<D>, const D: usize> Add<L, D> for ByteVariable {
             let carry_tmp = builder.and(l_xor_r, carry);
             bits[i] = builder.xor(l_xor_r, carry);
             carry = builder.or(carry_tmp, l_and_r);
+        }
+        ByteVariable(bits)
+    }
+}
+
+impl<L: PlonkParameters<D>, const D: usize> Sub<L, D> for ByteVariable {
+    type Output = Self;
+
+    fn sub(self, rhs: Self, builder: &mut CircuitBuilder<L, D>) -> Self::Output {
+        let mut bits = array![_ => builder.init::<BoolVariable>(); 8];
+        let mut borrow = builder.constant(false);
+        for i in 0..8 {
+            let l_xor_r = builder.xor(self.0[i], rhs.0[i]);
+            let not_r = builder.not(rhs.0[i]);
+            let l_and_not_r = builder.and(self.0[i], not_r);
+            let borrow_tmp = builder.and(l_xor_r, borrow);
+            bits[i] = builder.xor(l_xor_r, borrow);
+            borrow = builder.or(borrow_tmp, l_and_not_r);
         }
         ByteVariable(bits)
     }
