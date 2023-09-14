e90f2b6058038e0f06c32c323c374d52eb26c76c
John Guibas <john@succinct.xyz>
2023-09-14 15:08:06 -0700

    latest changes

--- plonky2x/src/frontend/eth/beacon/builder.rs
+++ plonky2x/src/frontend/eth/beacon/builder.rs
@@ -253,8 +253,8 @@ impl<L: PlonkParameters<D>, const D: usize> CircuitBuilder<L, D> {
         branch: &[Bytes32Variable],
         gindex: U64Variable,
     ) {
-        // let expected_root = self.ssz_restore_merkle_root(leaf, branch, gindex);
-        // self.assert_is_equal(root, expected_root);
+        let expected_root = self.ssz_restore_merkle_root(leaf, branch, gindex);
+        self.assert_is_equal(root, expected_root);
     }
 
     /// Verify a simple serialize (ssz) merkle proof with a constant index.
@@ -266,8 +266,8 @@ impl<L: PlonkParameters<D>, const D: usize> CircuitBuilder<L, D> {
         branch: &[Bytes32Variable],
         gindex: u64,
     ) {
-        // let expected_root = self.ssz_restore_merkle_root_const(leaf, branch, gindex);
-        // self.assert_is_equal(root, expected_root);
+        let expected_root = self.ssz_restore_merkle_root_const(leaf, branch, gindex);
+        self.assert_is_equal(root, expected_root);
     }
 
     /// Computes the expected merkle root given a leaf, branch, and dynamic index.
