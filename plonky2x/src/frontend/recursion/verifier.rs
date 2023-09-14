f11d304f71d1924ad25a0227a448c8dcc795a45f
Tamir Hemo <tamir@succinct.xyz>
2023-09-14 14:25:57 -0700

    delete commented module

--- plonky2x/src/frontend/recursion/verifier.rs
+++
@@ -1,5 +0,0 @@
-// /// A variable encoding the verifier circuit data for a plonky2x proof.
-// #[derive(Debug, Clone)]
-// pub struct VerifierVariable {
-//     pub constants_sigmas_cap: MerkleCapVariable,
-// }
