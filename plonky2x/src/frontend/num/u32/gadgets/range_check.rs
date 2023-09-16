db8ada68f309bb84f2815a95e169213a4d7e385f
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-09-15 17:32:07 -0700

    fix: break up u32_range_check gate into multiple rows

--- plonky2x/src/frontend/num/u32/gadgets/range_check.rs
+++ plonky2x/src/frontend/num/u32/gadgets/range_check.rs
@@ -13,11 +13,13 @@ pub fn range_check_u32_circuit<F: RichField + Extendable<D>, const D: usize>(
     builder: &mut CircuitBuilder<F, D>,
     vals: Vec<U32Target>,
 ) {
-    let num_input_limbs = vals.len();
-    let gate = U32RangeCheckGate::<F, D>::new(num_input_limbs);
-    let row = builder.add_gate(gate, vec![]);
+    vals.chunks(7).for_each(|chunk| {
+        let num_input_limbs = chunk.len();
+        let gate = U32RangeCheckGate::<F, D>::new(num_input_limbs);
+        let row = builder.add_gate(gate, vec![]);
 
-    for i in 0..num_input_limbs {
-        builder.connect(Target::wire(row, gate.wire_ith_input_limb(i)), vals[i].0);
-    }
+        for i in 0..num_input_limbs {
+            builder.connect(Target::wire(row, gate.wire_ith_input_limb(i)), chunk[i].0);
+        }
+    })
 }
