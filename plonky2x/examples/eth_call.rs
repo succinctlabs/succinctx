b1ca3d02c1bbcb0bd2bd926b276b7994982f9cd5
Uma Roy <uma.roy.us@gmail.com>
2023-09-27 20:54:42 -0700

    Added full eth_call example

--- plonky2x/examples/eth_call.rs
+++ plonky2x/examples/eth_call.rs
@@ -15,15 +15,44 @@
 //!
 //! Note that this circuit will not work with evm-based io.
 
+use std::env;
+
+use alloy_primitives::{Address, U160};
+use alloy_sol_types::{sol, SolType};
+use ethers::middleware::Middleware;
+use ethers::providers::{Http, Provider};
+use ethers::types::transaction::eip2718::TypedTransaction;
+use ethers::types::{Eip1559TransactionRequest, H160};
 use plonky2x::backend::circuit::{Circuit, PlonkParameters};
 use plonky2x::backend::function::{RustFunction, VerifiableRustFunction};
 use plonky2x::prelude::{CircuitBuilder, Variable};
+use tokio;
+type SolTuple = sol! { tuple(uint32, uint64, address, address, bytes) };
 
 struct EthCall {}
 
 impl RustFunction for EthCall {
     fn run(input_bytes: Vec<u8>) -> Vec<u8> {
-        input_bytes[0..32].to_vec()
+        let result = SolTuple::decode_single(&input_bytes, true).unwrap();
+        let (chain_id, block_number, from_address, to_address, calldata_) = result;
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(async {
+            dotenv::dotenv().ok();
+            let rpc_str = format!("RPC_{}", chain_id);
+            let rpc_url = env::var(rpc_str)
+                .unwrap_or_else(|_| panic!("RPC_{} environment variable was not found", chain_id));
+            let provider: Provider<Http> = Provider::<Http>::try_from(rpc_url).unwrap();
+            let mut tx = Eip1559TransactionRequest::new();
+            tx = tx.from::<H160>(from_address.0 .0.into());
+            tx = tx.to::<H160>(to_address.0 .0.into());
+            tx = tx.data(calldata_);
+
+            let result = provider
+                .call(&TypedTransaction::Eip1559(tx), Some(block_number.into()))
+                .await;
+            let bytes = result.unwrap();
+            bytes.0.into()
+        })
     }
 }
 
@@ -33,11 +62,25 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use alloy_primitives::{address, bytes};
+
     use super::*;
 
     #[test]
     fn test_eth_call() {
-        let output = EthCall::run(vec![1u8; 64]);
-        assert_eq!(output, vec![1u8; 32]);
+        let encoded_data = SolTuple::encode_single(&(
+            5u32,
+            9591261u64,
+            address!("355348b048b5b491793110bb76d7a2723262d175"),
+            address!("d555cd8277b0e16860f0ae44fcbc2ed94dfce9da"),
+            bytes!("050DDCCE00000000000000000000000003ACD25623E5999FDDC27BE7FDB904358700F3AE000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000064EB9D300000000000000000000000000000000000000000000000000000000000000000").to_vec()
+        ));
+
+        let output = EthCall::run(encoded_data);
+        let result = hex::encode(output);
+        assert_eq!(
+            result,
+            "000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000064eb9d90000000000000000000000000000000000000000000000000000048198737bd73000000000000000000000000000000000000000000000000000000006c6f600000000000000000000000000000000000000000000000021dcea0a4ea31b41370"
+        );
     }
 }
