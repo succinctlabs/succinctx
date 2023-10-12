a4a696af35d3894198fce2062f0478a2c5e3b1e6
Uma Roy <uma.roy.us@gmail.com>
2023-10-12 00:15:52 -0700

    clippy

--- plonky2x/core/src/backend/wrapper/wrap.rs
+++ plonky2x/core/src/backend/wrapper/wrap.rs
@@ -248,7 +248,6 @@ mod tests {
     use crate::backend::circuit::{DefaultParameters, Groth16WrapperParameters};
     use crate::frontend::builder::CircuitBuilder;
     use crate::frontend::hash::sha::sha256::sha256;
-    use crate::prelude::*;
     use crate::utils;
 
     fn to_bits(msg: Vec<u8>) -> Vec<bool> {
