d3e5ab6d256b8ba827ae98baaecdfc0e2bd3e9cd
Uma Roy <uma.roy.us@gmail.com>
2023-10-11 22:39:32 -0700

    Clean up verifier test

--- plonky2x/verifier/verifier_test.go
+++ plonky2x/verifier/verifier_test.go
@@ -15,7 +15,7 @@ import (
 func TestPlonky2xVerifierCircuit(t *testing.T) {
 	assert := test.NewAssert(t)
 
-	testCase := func() error {
+	testCase := func(option int64) error {
 		dummyCircuitPath := "./data/dummy"
 		circuitPath := "./data/test_circuit"
 
@@ -51,53 +51,31 @@ func TestPlonky2xVerifierCircuit(t *testing.T) {
 			InputHash:      frontend.Variable(inputHash),
 			OutputHash:     frontend.Variable(outputHash),
 		}
-		return test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
-	}
 
-	assert.NoError(testCase())
-}
-
-func TestPlonky2xVerifierCircuitFails(t *testing.T) {
-	assert := test.NewAssert(t)
-
-	testCase := func() error {
-		dummyCircuitPath := "./data/dummy"
-		circuitPath := "./data/test_circuit"
-
-		verifierOnlyCircuitDataDummy := variables.DeserializeVerifierOnlyCircuitData(
-			types.ReadVerifierOnlyCircuitData(dummyCircuitPath + "/verifier_only_circuit_data.json"),
-		)
-		proofWithPisDummy := variables.DeserializeProofWithPublicInputs(
-			types.ReadProofWithPublicInputs(dummyCircuitPath + "/proof_with_public_inputs.json"),
-		)
-		commonCircuitDataDummy := types.ReadCommonCircuitData(dummyCircuitPath + "/common_circuit_data.json")
-
-		circuit := Plonky2xVerifierCircuit{
-			ProofWithPis:      proofWithPisDummy,
-			VerifierData:      verifierOnlyCircuitDataDummy,
-			VerifierDigest:    frontend.Variable(0), // Can be empty for defining the circuit
-			InputHash:         frontend.Variable(0),
-			OutputHash:        frontend.Variable(0),
-			CommonCircuitData: commonCircuitDataDummy,
-		}
-
-		verifierOnlyCircuitData := variables.DeserializeVerifierOnlyCircuitData(
-			types.ReadVerifierOnlyCircuitData(circuitPath + "/verifier_only_circuit_data.json"),
-		)
-		proofWithPis := types.ReadProofWithPublicInputs(circuitPath + "/proof_with_public_inputs.json")
-		inputHash, outputHash := GetInputHashOutputHash(proofWithPis)
-
-		proofWithPisVariable := variables.DeserializeProofWithPublicInputs(proofWithPis)
-
-		witness := Plonky2xVerifierCircuit{
-			ProofWithPis:   proofWithPisVariable,
-			VerifierData:   verifierOnlyCircuitData,
-			VerifierDigest: verifierOnlyCircuitData.CircuitDigest,
-			InputHash:      frontend.Variable(inputHash),
-			OutputHash:     frontend.Variable(outputHash),
+		// When an option is turned on, we fuzz the different witness values to check
+		// that the circuit proof generation will be invalid when the inputs are incorrect.
+		if option == 1 {
+			witness.InputHash = frontend.Variable(0)
+		} else if option == 2 {
+			witness.OutputHash = frontend.Variable(0)
+		} else if option == 3 {
+			witness.VerifierDigest = verifierOnlyCircuitDataDummy.CircuitDigest
+		} else if option == 4 {
+			witness.VerifierDigest = frontend.Variable(0)
+		} else if option == 5 {
+			witness.ProofWithPis = proofWithPisDummy
+		} else if option == 6 {
+			witness.VerifierData = verifierOnlyCircuitDataDummy
+		} else if option == 7 {
+			witness.ProofWithPis = proofWithPisDummy
+			witness.VerifierData = verifierOnlyCircuitDataDummy
+			witness.VerifierDigest = verifierOnlyCircuitDataDummy.CircuitDigest
 		}
 		return test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
 	}
 
-	assert.NoError(testCase())
+	assert.NoError(testCase(0))
+	for i := 1; i <= 7; i++ {
+		assert.Error(testCase(int64(i)))
+	}
 }
