267e6b2bc9057c1f602b6858d573749b6c635352
Chris Tian <chris@succinct.xyz>
2023-10-12 17:09:40 -0700

    fix

--- circomx/dist/src/bigint.d.ts
+++
@@ -1,4 +0,0 @@
-import { PointG1 } from "@noble/bls12-381";
-export declare function pointToBigInt(point: PointG1): [bigint, bigint];
-export declare function bigIntToArray(n: number, k: number, x: bigint): string[];
-//# sourceMappingURL=bigint.d.ts.map
