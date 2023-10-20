1cd05d13d7ba2ee93dfe7bf8cd457b1a189f676d
Kevin Jue <kjue235@gmail.com>
2023-10-20 13:17:44 -0700

    removed projectivepoint

--- plonky2x/core/src/frontend/ecc/ed25519/curve/curve_types.rs
+++ plonky2x/core/src/frontend/ecc/ed25519/curve/curve_types.rs
@@ -22,12 +22,6 @@ pub trait Curve: 'static + Sync + Sized + Copy + Debug {
 
     const GENERATOR_AFFINE: AffinePoint<Self>;
 
-    const GENERATOR_PROJECTIVE: ProjectivePoint<Self> = ProjectivePoint {
-        x: Self::GENERATOR_AFFINE.x,
-        y: Self::GENERATOR_AFFINE.y,
-        z: Self::BaseField::ONE,
-    };
-
     fn convert(x: Self::ScalarField) -> CurveScalar<Self> {
         CurveScalar(x)
     }
@@ -78,21 +72,6 @@ impl<C: Curve> AffinePoint<C> {
     pub fn is_valid(&self) -> bool {
         let Self { x, y, zero } = *self;
         zero || C::A * x.square() + y.square() == C::BaseField::ONE + C::D * x.square() * y.square()
-    }
-
-    pub fn to_projective(&self) -> ProjectivePoint<C> {
-        let Self { x, y, zero } = *self;
-        let z = if zero {
-            C::BaseField::ZERO
-        } else {
-            C::BaseField::ONE
-        };
-
-        ProjectivePoint { x, y, z }
-    }
-
-    pub fn batch_to_projective(affine_points: &[Self]) -> Vec<ProjectivePoint<C>> {
-        affine_points.iter().map(Self::to_projective).collect()
     }
 
     #[must_use]
@@ -160,153 +139,12 @@ impl<C: Curve> Hash for AffinePoint<C> {
     }
 }
 
-/// A point on a twisted Edwards curve, represented in projective coordinates.
-#[derive(Copy, Clone, Debug)]
-pub struct ProjectivePoint<C: Curve> {
-    pub x: C::BaseField,
-    pub y: C::BaseField,
-    pub z: C::BaseField,
-}
-
-impl<C: Curve> ProjectivePoint<C> {
-    pub const ZERO: Self = Self {
-        x: C::BaseField::ZERO,
-        y: C::BaseField::ONE,
-        z: C::BaseField::ZERO,
-    };
-
-    pub fn nonzero(x: C::BaseField, y: C::BaseField, z: C::BaseField) -> Self {
-        let point = Self { x, y, z };
-        debug_assert!(point.is_valid());
-        point
-    }
-
-    // Taken from here:  https://en.wikipedia.org/wiki/Twisted_Edwards_curve#Extended_coordinates
-    pub fn is_valid(&self) -> bool {
-        let Self { x, y, z } = *self;
-        z.is_zero()
-            || (C::A * x.square() + y.square()) * z.square()
-                == z.square().square() + C::D * x.square() * y.square()
-    }
-
-    pub fn to_affine(&self) -> AffinePoint<C> {
-        let Self { x, y, z } = *self;
-        if z == C::BaseField::ZERO {
-            AffinePoint::ZERO
-        } else {
-            let z_inv = z.inverse();
-            AffinePoint::nonzero(x * z_inv, y * z_inv)
-        }
-    }
-
-    pub fn batch_to_affine(proj_points: &[Self]) -> Vec<AffinePoint<C>> {
-        let n = proj_points.len();
-        let zs: Vec<C::BaseField> = proj_points.iter().map(|pp| pp.z).collect();
-        let z_invs = C::BaseField::batch_multiplicative_inverse(&zs);
-
-        let mut result = Vec::with_capacity(n);
-        for i in 0..n {
-            let Self { x, y, z } = proj_points[i];
-            result.push(if z == C::BaseField::ZERO {
-                AffinePoint::ZERO
-            } else {
-                let z_inv = z_invs[i];
-                AffinePoint::nonzero(x * z_inv, y * z_inv)
-            });
-        }
-        result
-    }
-
-    // https://en.wikipedia.org/wiki/Twisted_Edwards_curve#Doubling_on_projective_twisted_curves
-    #[must_use]
-    pub fn double(&self) -> Self {
-        let Self { x, y, z } = *self;
-        if z == C::BaseField::ZERO {
-            return ProjectivePoint::ZERO;
-        }
-
-        let b = (x + y).square();
-        let c = x.square();
-        let d = y.square();
-        let e = C::A * c;
-        let f = e + d;
-        let h = z.square();
-        let j = f - h.double();
-        let x3 = (b - c - d) * j;
-        let y3 = f * (e - d);
-        let z3 = f * j;
-
-        let double = ProjectivePoint::<C> {
-            x: x3,
-            y: y3,
-            z: z3,
-        };
-        debug_assert!(double.is_valid());
-
-        Self {
-            x: x3,
-            y: y3,
-            z: z3,
-        }
-    }
-
-    pub fn add_slices(a: &[Self], b: &[Self]) -> Vec<Self> {
-        assert_eq!(a.len(), b.len());
-        a.iter()
-            .zip(b.iter())
-            .map(|(&a_i, &b_i)| a_i + b_i)
-            .collect()
-    }
-
-    #[must_use]
-    pub fn neg(&self) -> Self {
-        Self {
-            x: -self.x,
-            y: self.y,
-            z: self.z,
-        }
-    }
-}
-
-impl<C: Curve> PartialEq for ProjectivePoint<C> {
-    fn eq(&self, other: &Self) -> bool {
-        let ProjectivePoint {
-            x: x1,
-            y: y1,
-            z: z1,
-        } = *self;
-        let ProjectivePoint {
-            x: x2,
-            y: y2,
-            z: z2,
-        } = *other;
-        if z1 == C::BaseField::ZERO || z2 == C::BaseField::ZERO {
-            return z1 == z2;
-        }
-
-        // We want to compare (x1/z1, y1/z1) == (x2/z2, y2/z2).
-        // But to avoid field division, it is better to compare (x1*z2, y1*z2) == (x2*z1, y2*z1).
-        x1 * z2 == x2 * z1 && y1 * z2 == y2 * z1
-    }
-}
-
-impl<C: Curve> Eq for ProjectivePoint<C> {}
-
 impl<C: Curve> Neg for AffinePoint<C> {
     type Output = AffinePoint<C>;
 
     fn neg(self) -> Self::Output {
         let AffinePoint { x, y, zero } = self;
         AffinePoint { x: -x, y, zero }
-    }
-}
-
-impl<C: Curve> Neg for ProjectivePoint<C> {
-    type Output = ProjectivePoint<C>;
-
-    fn neg(self) -> Self::Output {
-        let ProjectivePoint { x, y, z } = self;
-        ProjectivePoint { x: -x, y, z }
     }
 }
 
