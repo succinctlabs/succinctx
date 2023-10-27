fa77412f0716468b30050b8b12835b15305b0007
Matt Stam <mattstam@live.com>
2023-10-27 14:34:49 -0700

    chore: update contract bindings and addresses (#261)

---
+++ scripts/release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+if ! echo "$1" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+(-rc\.[0-9]+)?$'; then
+  echo "${1} is not in MAJOR.MINOR.PATCH format"
+  exit 1
+fi
+
+# Create a new signed annotated tag for the version
+git tag "v${1}" -as -m "release v${1}"
+
+# Push all tags, which triggers the Release workflow
+git push --atomic origin "v${1}" --force
