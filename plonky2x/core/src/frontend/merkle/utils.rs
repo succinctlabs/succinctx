3aec805e7b7de3210c431f442b12baca507c5644
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-11-07 19:27:31 -0800

    ceil base2 log docs

--- plonky2x/core/src/frontend/merkle/utils.rs
+++ plonky2x/core/src/frontend/merkle/utils.rs
@@ -1,13 +1,16 @@
+/// Computes the ceiling of the base 2 log of a `usize`.
 pub fn log2_ceil_usize(x: usize) -> usize {
     if x <= 1 {
-        return 0; // log2(0) and log2(1) are both 0
+        // log2(0) and log2(1) are both 0.
+        return 0;
     }
 
     let mut result = 0;
-    let mut value = x - 1; // Subtract 1 to ensure rounding up for powers of 2
+    // Subtract 1 to ensure rounding up for powers of 2.
+    let mut value = x - 1;
 
     while value > 0 {
-        value >>= 1; // Right shift by 1, equivalent to dividing by 2
+        value >>= 1;
         result += 1;
     }
 
