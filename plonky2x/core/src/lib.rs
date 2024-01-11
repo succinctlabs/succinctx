ce250898a56a1c6eb027639779ce40333bcf5330
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-01-11 15:46:57 -0800

    re-export

--- plonky2x/core/src/lib.rs
+++ plonky2x/core/src/lib.rs
@@ -15,8 +15,6 @@ pub mod utils;
 
 pub mod prelude {
     pub use curta::math::prelude::cubic::element::CubicElement;
-    // Re-export plonky2
-    pub use plonky2;
     pub use plonky2::field::extension::Extendable;
     pub use plonky2::field::goldilocks_field::GoldilocksField;
     pub use plonky2::field::types::Field;
@@ -24,6 +22,8 @@ pub mod prelude {
     pub use plonky2::iop::target::Target;
     pub use plonky2::iop::witness::{PartialWitness, Witness, WitnessWrite};
     pub use plonky2::plonk::config::PoseidonGoldilocksConfig;
+    // Re-export plonky2
+    pub use plonky2::*;
     pub use plonky2x_derive::CircuitVariable;
 
     pub use crate::backend::circuit::config::{DefaultParameters, PlonkParameters};
