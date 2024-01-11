3b455cf96381fe72ca12f80f4879350aa7c11c8f
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-01-11 15:43:47 -0800

    re-export

--- plonky2x/core/src/lib.rs
+++ plonky2x/core/src/lib.rs
@@ -15,6 +15,8 @@ pub mod utils;
 
 pub mod prelude {
     pub use curta::math::prelude::cubic::element::CubicElement;
+    // Re-export plonky2
+    pub use plonky2;
     pub use plonky2::field::extension::Extendable;
     pub use plonky2::field::goldilocks_field::GoldilocksField;
     pub use plonky2::field::types::Field;
