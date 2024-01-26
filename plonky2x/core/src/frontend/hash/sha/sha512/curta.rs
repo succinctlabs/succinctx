bd6a1bb782f5ab7c8c3e08cedd640a7d69484f36
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-01-25 22:45:52 -0800

    logging

--- plonky2x/core/src/frontend/hash/sha/sha512/curta.rs
+++ plonky2x/core/src/frontend/hash/sha/sha512/curta.rs
@@ -178,6 +178,8 @@ impl<L: PlonkParameters<D>, const D: usize> CircuitBuilder<L, D> {
 
 #[cfg(test)]
 mod tests {
+    use std::env;
+
     use rand::{thread_rng, Rng};
 
     use crate::prelude::*;
@@ -265,9 +267,14 @@ mod tests {
         test_sha512_variable_length(&msg, 0, expected_digest);
     }
 
+    // FAILED
     #[test]
     #[cfg_attr(feature = "ci", ignore)]
     fn test_sha512_curta_variable_large_message() {
+        env::set_var("RUST_LOG", "debug");
+        env_logger::try_init().unwrap_or_default();
+        dotenv::dotenv().ok();
+
         let mut msg : Vec<u8> = bytes!("35c323757c20640a294345c89c0bfcebe3d554fdb0c7b7a0bdb72222c531b1ecf7ec1c43f4de9d49556de87b86b26a98942cb078486fdb44de38b80864c3973153756363696e6374204c616273");
         let len = msg.len() as u32;
         msg.resize(256, 1);
@@ -276,9 +283,14 @@ mod tests {
         test_sha512_variable_length(&msg, len, expected_digest);
     }
 
+    // FAILED
     #[test]
     #[cfg_attr(feature = "ci", ignore)]
     fn test_sha512_curta_variable_short_message_same_slice() {
+        env::set_var("RUST_LOG", "debug");
+        env_logger::try_init().unwrap_or_default();
+        dotenv::dotenv().ok();
+
         let mut msg: Vec<u8> = b"plonky2".to_vec();
         let len = msg.len() as u32;
         msg.resize(128, 1);
@@ -287,9 +299,14 @@ mod tests {
         test_sha512_variable_length(&msg, len, expected_digest);
     }
 
+    // FAILED
     #[test]
     #[cfg_attr(feature = "ci", ignore)]
     fn test_sha512_curta_variable_short_message_different_slice() {
+        env::set_var("RUST_LOG", "debug");
+        env_logger::try_init().unwrap_or_default();
+        dotenv::dotenv().ok();
+
         let mut msg: Vec<u8> = b"plonky2".to_vec();
         let len = msg.len() as u32;
         msg.resize(512, 1);
