9ea7f0cdaf8aa9e136117678a2370e1ed1837f70
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-02-28 14:53:12 -0800

    docs: output reader (#375)

--- contracts/src/libraries/OutputReader.sol
+++ contracts/src/libraries/OutputReader.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.16;
 
+// OutputReader should be used for reading encodePacked output uint256, uint128, uint64, uint32.
 library OutputReader {
     function readUint256(bytes memory _output, uint256 _offset)
         internal
@@ -20,6 +21,8 @@ library OutputReader {
     {
         uint128 value;
         assembly {
+            // Reading into u128 only parses the lowest 16 bytes of slot, this is where
+            // encodePacked writes the uint128.
             value := mload(add(add(_output, 0x10), _offset))
         }
         return (_offset + 16, value);
@@ -32,6 +35,8 @@ library OutputReader {
     {
         uint64 value;
         assembly {
+            // Reading into u64 only parses the lowest 8 bytes of slot, this is where
+            // encodePacked writes the uint64.
             value := mload(add(add(_output, 0x08), _offset))
         }
         return (_offset + 8, value);
@@ -44,6 +49,8 @@ library OutputReader {
     {
         uint32 value;
         assembly {
+            // Reading into u32 only parses the lowest 4 bytes of slot, this is where
+            // encodePacked writes the uint32.
             value := mload(add(add(_output, 0x04), _offset))
         }
         return (_offset + 4, value);
